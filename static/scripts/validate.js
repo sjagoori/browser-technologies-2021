d7eff220e626b60c0626d799135b3e81f80ff240
sjagoori <sjagoori@users.noreply.github.com>
2021-03-25 12:53:05 +0100

    + validation script

--- static/scripts/validate.js
+++ static/scripts/validate.js
@@ -1,1 +1,12 @@
 console.log('validator running')
+
+const form = document.getElementById('enquette').addEventListener('submit', e => handleForm(e))
+
+function handleForm(e){
+  e.preventDefault() // delete when done validating
+  
+  let formAction = e.target.getAttribute("action");
+  let activeElementAction = document.activeElement.getAttribute("formaction");
+  let action = activeElementAction || formAction;
+  console.log(action);
+}
