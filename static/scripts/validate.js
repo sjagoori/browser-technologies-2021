41f534551be21d82e5c62c78be1fc04964ecaca2
sjagoori <sjagoori@users.noreply.github.com>
2021-03-31 14:02:23 +0200

    +

--- static/scripts/validate.js
+++ static/scripts/validate.js
@@ -1,4 +1,19 @@
-console.log('validator running');
+const form = document.getElementById('enquette')
+form.addEventListener('change', e => handleForm(e))
+form.children[form.children.length - 1].remove()
+
+const naam = document.getElementById('name')
+naam.setCustomValidity('Voer hier je voor en/of achternaam in!')
+
+const studentnummer = document.getElementById('studentnummer')
+studentnummer.required = true;
+studentnummer.setCustomValidity('Voer hier je studentnummer in, bijvoorbeeld: 50012356789')
+studentnummer.addEventListener('change', (e) => { if (e.target.value.length < 9) e.path[0].reportValidity() })
+
+const submitButtons = document.querySelectorAll('[formaction]');
+Object.values(submitButtons).map(key => key.remove())
+const inputs = document.getElementsByTagName('input');
+const navigationButtons = document.getElementsByTagName('a')
 
 setCache();
 bindFieldsetEvents();
@@ -13,9 +28,6 @@ function bindFieldsetEvents() {
       ? (key.addEventListener('change', e => handleFieldset(e, key.id), prefillCache()))
       : false)
     .filter(elem => typeof elem == 'object');
-
-  // Object.values(document.getElementsByTagName('fieldset'))
-  // .map(key => key.id ? checkFilled(key.id) : null)
 }
 
 /**
@@ -28,8 +40,6 @@ function handleFieldset(e, parentId) {
   let worker = localStorage.getItem(parentId)
     ? JSON.parse(localStorage.getItem(parentId))
     : (localStorage.setItem(parentId, JSON.stringify({ data: {} })), JSON.parse(localStorage.getItem(parentId)));
-
-  // console.log(e.target.value);
 
   worker.data[`${e.target.name}`] = e.target.value;
   localStorage.setItem(parentId, JSON.stringify(worker));
@@ -99,20 +109,8 @@ function checkFilled(pick) {
   return checkEmptyString > 0 || checkEmptyObject > 0 ? false : true
 }
 
-
-
-const form = document.getElementById('enquette')
-form.addEventListener('change', e => handleForm(e))
-form.children[form.children.length - 1].remove()
-
-const submitButtons = document.querySelectorAll('[formaction]');
-Object.values(submitButtons).map((key, index) => index == (submitButtons.length - 1) ? key.textContent = 'Submit' : key.remove())
-
-const inputs = document.getElementsByTagName('input');
-const navigationButtons = document.getElementsByTagName('a')
-
 function handleForm(e) {
-  e.preventDefault() // 
+  e.preventDefault()
 
   let parentElement = e.target.parentElement.parentElement.parentElement
   let nextSibling = parentElement.nextElementSibling ? parentElement.nextElementSibling.id : parentElement.id
@@ -123,7 +121,30 @@ function handleForm(e) {
     navButton ? navButton.style.textDecoration = 'line-through' : null
   }
 
-  return checkAll() ? true : false
+  let myForm = document.getElementById('enquette');
+
+  let formData = new FormData(myForm);
+  console.log(formData);
+
+  if (checkAll()) {
+    sendOff()
+    alert('saved')
+  }
+}
+
+function sendOff() {
+  localStorage.setItem('name', naam.value ? JSON.stringify({ data: { name: naam.value } }) : JSON.stringify({ data: 'Naam' }))
+  localStorage.setItem('studentNummer', studentnummer.value ? JSON.stringify({ data: { studentnummer: studentnummer.value } }) : JSON.stringify({ data: '500761767' }))
+
+  fetch('/handleFetch', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(localStorage)
+  })
+  return true;
 }
 
 function checkAll() {
@@ -147,12 +168,6 @@ function checkAll() {
   return empty > 0 ? false : true
 }
 
-form.onformdata = (e) => {
-  console.log('formdata fired');
-}
-A
-
-
 function checkText(input) {
   return input.value.length == 0 ? generateError(input, input.title) : true
 }
@@ -161,7 +176,6 @@ function checkNumber(input) {
   // console.log(input);
   return input.value.length == 0 || input.value != input.pattern ? generateError(input, 'kiezen moet je') : true
 }
-
 
 function generateError(input, errorMessage) {
   let error = document.createElement('span');
